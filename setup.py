874862b3d7c93154c51812fbedbc96668120b766
Beshr Kayali <Beshr Kayali@.(none)>
2009-04-21 22:04:47 +0200

    pytodo goes to github world

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.1'
+README = os.path.join(os.path.dirname(__file__),'README')
+long_description = open(README).read() + '\n\n'
+
+setup(name='pytodo',
+	  version = version,
+	  description = 'TODO file generator from python scripts',
+	  classifiers = ["Programming Language :: Python",
+	  				 "Intended Audience :: Developers",
+	  				 "License :: GPL",	  				 
+	  				],
+	  keywords='todo pytodo',
+	  author='Beshr Kayali',
+	  author_email='beshrkayali@gmail.com',
+	  url='http://github.com/beshrkayali/pytodo',
+	  license='GPL',
+	  packages=find_packages(),
+	  namespace_packages=['pytodo'],
+	  )
